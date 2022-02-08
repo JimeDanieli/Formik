4995873609e8a6c323f16a50b147aff5d52ac274
Jime <jimedanieli.09@gmail.com>
2022-02-08 19:01:10 -0300

    errors working

--- formik/src/App.js
+++ formik/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 //useFormik recibe como argumento un objeto y con ese objeto nosotros podemos indicar varios parametros de configuracion
 //el primero que le queremos pasar son los valores iniciales, en vez de useState, vamos a usar initialValues, que contiene otro objeto literal que va a contener los valores de los campos de nuestros formularios y estos van acompañados de los nombres de los formularios como su llave
 //onSubmit recibe los valores que tiene nuestro formulario
+//a su vez no tenemos que prevenir por defecto porque useFormik hace eso por nosotros
 function App() {
   const formik= useFormik({
     initialValues:{
@@ -10,12 +11,56 @@ function App() {
       lastname:'',
       email:'',
     },
+    //no se ejecuta hasta que no toquemos el campo
+    validate:(values)=>{
+        const errors={}
+
+        if(!values.name){
+        errors.name='Required'
+        }  
+        if(!values.lastname){
+          errors.lastname='Last name is required'
+        }
+        if(!values.email){
+          errors.email='E-mail is required'
+        }
+        return errors
+
+      
+    },
     onSubmit: values=> console.log(values)
   })
   return (
-    <div className="App">
-     
-    </div>
+    <form onSubmit={formik.handleSubmit}>
+        <label>Name</label>
+        <input
+          name='name'
+          type='text'
+          onChange={formik.handleChange} 
+          value={formik.values.name}
+        />
+        {formik.errors.name ? <div>{formik.errors.name}</div> :null}
+          <br />
+        <label>Last name</label>
+        <input
+          name='lastname'
+          type='text'
+          onChange={formik.handleChange} 
+          value={formik.values.lastname}
+        />
+        {formik.errors.lastname ? <div>{formik.errors.lastname}</div>: null}
+          <br />
+        <label>E-mail</label>
+        <input
+          name='email'
+          type='text'
+          onChange={formik.handleChange} 
+          value={formik.values.email}
+        />
+        {formik.errors.email ? <div>{formik.errors.email}</div> : null}
+          <br />
+        <button type='submit'>Send</button>
+    </form>
   );
 }
 
